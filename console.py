8e0d3672eda9f36934f34a31f08de337b579b218
MuhammadAfify <muhammadafefy@gmail.com>
2024-03-11 18:24:25 -0700

    AirBnB

---
+++ console.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Define the HBnB console"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Defines the HolbertonBnB command interpreter.
+    Attributes:
+        prompt (str): The command prompt.
+    """
+
+    prompt = "(hbnb) "
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
+    
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF signal to exit the program."""
+        print("")
+        return True
+    
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
