6a0c712e9cfcdd65aaf21af39e225428eef0cb92
MuhammadAfify <muhammadafefy@gmail.com>
2024-05-19 19:45:34 +0000

    web flask

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -29,6 +29,7 @@ def pythoniscool(text='is cool'):
     """display “Python ”, followed by the value of the text variable"""
     return 'Python ' + text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def imanumber(n):
     """display “n is a number” only if n is an integer"""
